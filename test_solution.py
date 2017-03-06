409645428396ce3bf98364289d8ad4b0aa405eab
joantyry <tyry7979@gmail.com>
2017-03-06 22:45:55 +0300

    Delete test_solution.py

--- test_solution.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from tdd import solution
-class TestSolution(unittest.TestCase):
-	def test_addition(self,):
-		self.assertTrue(solution(10,20,"+"),30)
-
-
-if __name__ =="__main__":
-	unittest.main()
